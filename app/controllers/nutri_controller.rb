15712d14ea4cce7cc35f6c31ea0e3e15354e814a
Ruhan <ruhandutra.reis@gmail.com>
2022-02-14 20:39:28 -0400

    adição da gem mechanize

--- app/controllers/nutri_controller.rb
+++ app/controllers/nutri_controller.rb
@@ -1,5 +1,10 @@
 class NutriController < ApplicationController
   def buscar_produto
+    termo = params[:produto_1]
+    agent = Mechanize.new
+    page = agent.get("https://www.myfitnesspal.com/pt/food/search?search=#{termo}")
+
+    byebug
     redirect_to root_path(produtos: ['asdf', 'asdf'])
   end
 
